540b1746135726480b4282bf2faf9e72e9ab37ca
Sören Irlenbusch <blackphoenix21.dark@gmail.com>
2025-03-24 14:25:14 +0100

    lvl3

---
+++ Assets/Scripts/MoveTileGod.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTileGod : MonoBehaviour
+{
+    private GameObject statue;
+    private int tileName;
+    private Vector3 finishPos;
+    private float floater = 1f;
+    private bool moveable = false;
+    void Start()
+    {
+        statue = GameObject.Find("Statue");
+        string name = gameObject.GetComponent<SpriteRenderer>().sprite.name;
+        tileName = int.Parse(name.Split("1_")[1]) - 1;
+        Debug.Log(tileName);
+        finishPos = (Vector3)statue.GetComponent<TileFinish>().finsihedTilePos[tileName];
+    }
+
+    void Update()
+    {
+        if (gameObject.transform.position.x > finishPos.x - floater && gameObject.transform.position.x < finishPos.x + floater &&
+            gameObject.transform.position.y > finishPos.y - floater && gameObject.transform.position.y < finishPos.y + floater)
+        {
+            gameObject.transform.position = new Vector3(finishPos.x, finishPos.y, -0.02f);
+        }
+        if (moveable)
+        {
+            Move();
+        }
+    }
+
+    void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (gameObject.transform.position == new Vector3(finishPos.x, finishPos.y, -0.02f))
+            {
+                return;
+            }
+            if (moveable)
+            {
+                moveable = false;
+            }
+            else
+            {
+                moveable = true;
+            }
+        }
+    }
+
+    void Move()
+    {
+        gameObject.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+        Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+    }
+}
---
+++ Assets/Scripts/TileFinish.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileFinish : MonoBehaviour
+{
+    public Vector2[] finsihedTilePos = new Vector2[7] { new Vector2(0.11f, 3.51f), new Vector2(-2.08f, 2.23f),
+    new Vector2(1.1f, 0.48f), new Vector2(-1.61f, 0.42f), new Vector2(1.68f, -0.76f), new Vector2(-0.92f, -0.72f),
+    new Vector2(1.56f,-2.43f) };
+
+}
