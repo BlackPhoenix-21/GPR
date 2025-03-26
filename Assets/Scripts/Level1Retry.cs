b055bd45a2380bd846c4d3c2fbb0047e5cdcda77
Sören Irlenbusch <blackphoenix21.dark@gmail.com>
2025-03-26 13:54:09 +0100

    smort

--- Assets/Scripts/Level1Retry.cs
+++ Assets/Scripts/Level1Retry.cs
@@ -1,25 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using AC;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Level1Retry : MonoBehaviour
 {
-    public string sceneName = "Level-1";
-    // Start is called before the first frame update
+    private string sceneName;
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        sceneName = GlobalVariables.GetStringValue(0);
     }
 
     public void SceneChange()
     {
-        SceneManager.LoadScene(sceneName);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
     }
 }
