62d836494aa432d8f9fde1dcf0906617d488c64c
Sören Irlenbusch <blackphoenix21.dark@gmail.com>
2025-03-30 18:08:23 +0200

    lvl 3

--- Assets/Scripts/TileFinishM.cs
+++ Assets/Scripts/TileFinishM.cs
@@ -11,6 +11,7 @@ public class TileFinishM : MonoBehaviour
     public bool isFinished = false;
     private GameObject acObject;
     private ActionParameter myParameter;
+    private bool added = false;
     void Start()
     {
         finsihedTilePos = new Vector2(gameObject.transform.localPosition.x, gameObject.transform.localPosition.y);
@@ -23,11 +24,24 @@ public class TileFinishM : MonoBehaviour
         myParameter = myActionList.GetParameter("Spiegelnum");
         if (myParameter.intValue != 0)
         {
-
+            //Debug.Log("SpiegelNum: " + myParameter.intValue);
         }
         else
         {
-
+            //Debug.Log("SpiegelNum: 0");
         }
+
+        if (isFinished && !added)
+        {
+            StartCoroutine(FinishedM());
+            added = true;
+        }
+    }
+    IEnumerator FinishedM()
+    {
+        yield return new WaitForSeconds(0.75f);
+        GameObject.Find("Exit: Use").GetComponent<ActionList>().RunFromIndex(0);
+        GameObject.Find("SpiegelMe").GetComponent<ActionList>().GetParameter("SpiegelAnzahl").intValue += 1;
+        GameObject.Find("SpiegelMe").GetComponent<ActionList>().RunFromIndex(0);
     }
 }
